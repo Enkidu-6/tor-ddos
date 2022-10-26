ebe5064a3c3ecbb58d606df68ed38d1ce4cc0251
Enkidu <101878139+Enkidu-6@users.noreply.github.com>
2022-10-26 01:17:49 -0400

    Finds Tor relays in the block list

---
+++ test/dual-or/compare.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# set -x
+ipset -L tor-ddos | awk '{print $1}' > /var/tmp/file1
+ipset -L persec | awk '{print $1}' >> /var/tmp/file1
+curl https://wcbsecurity.com/relays.txt > /var/tmp/file2
+cd /var/tmp
+perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  file1 file2
