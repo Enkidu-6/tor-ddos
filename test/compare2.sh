16dbe411142a617c129567f83b533101079c1b7d
Enkidu <101878139+Enkidu-6@users.noreply.github.com>
2022-10-26 00:47:45 -0400

    Delete compare2.sh

--- test/compare2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# set -x
-ipset -L tor2-ddos | awk '{print $1}' > /var/tmp/file3
-ipset -L persec2 | awk '{print $1}' >> /var/tmp/file3
-curl https://wcbsecurity.com/relays.txt > /var/tmp/file2
-cd /var/tmp
-perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  file3 file2
