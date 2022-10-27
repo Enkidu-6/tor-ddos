234aaad0c3b49bed2caf55e1015eb668ef504dc3
Enkidu <101878139+Enkidu-6@users.noreply.github.com>
2022-10-27 19:30:12 -0400

    Update remove2.sh

--- cron/remove2.sh
+++ cron/remove2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # set -x
-ipset -L tor2-ddos | awk '{print $1}' > /var/tmp/file3
+/usr/sbin/ipset -L tor2-ddos | awk '{print $1}' > /var/tmp/file3
 curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/relays-v4.txt' > /var/tmp/file2
 cd /var/tmp
 perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  file3 file2 > remove2
 for i in `cat remove2` ;
 do
-ipset del tor2-ddos $i
+/usr/sbin/ipset del tor2-ddos $i
 done;
