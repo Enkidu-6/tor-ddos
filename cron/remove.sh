fe6a442c336568835cc8bee46c930b0053024dbc
Enkidu <101878139+Enkidu-6@users.noreply.github.com>
2022-10-27 04:21:17 -0400

    Update remove.sh

--- cron/remove.sh
+++ cron/remove.sh
@@ -2,8 +2,8 @@
 # set -x
 ipset -L tor-ddos | awk '{print $1}' > /var/tmp/file1
 curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/relays-v4.txt' > /var/tmp/file2
-perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  file1 file2 > /var/tmp/remove
-cd /var/tmp ;
+cd /var/tmp
+perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  file1 file2 > remove
 for i in `cat remove` ;
 do
 ipset del tor-ddos $i
