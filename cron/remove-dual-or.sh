8da7f935e59d652c9948f0bef7aaf1c625522328
Enkidu <101878139+Enkidu-6@users.noreply.github.com>
2022-10-27 19:27:08 -0400

    Update remove-dual-or.sh

--- cron/remove-dual-or.sh
+++ cron/remove-dual-or.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # set -x
-ipset -L tor-ddos | awk '{print $1}' > /var/tmp/file1
+/usr/sbin/ipset -L tor-ddos | awk '{print $1}' > /var/tmp/file1
 curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/dual-or.txt' > /var/tmp/dual-or
 cd /var/tmp
 perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  file1 dual-or > dual
 for i in `cat dual` ;
 do
-ipset del tor-ddos $i
+/usr/sbin/ipset del tor-ddos $i
 done;
