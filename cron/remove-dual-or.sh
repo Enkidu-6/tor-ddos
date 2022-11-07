c99c898e1243ebc7a8062061fc3a2bc26ed53797
Enkidu <101878139+Enkidu-6@users.noreply.github.com>
2022-11-07 07:19:59 -0500

    Update remove-dual-or.sh

--- cron/remove-dual-or.sh
+++ cron/remove-dual-or.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # set -x
 /usr/sbin/ipset -L tor-ddos | awk '{print $1}' > /var/tmp/file1
-if [[ ! -e /var/tmp/file2 ]]; then
+if [[ ! -e /var/tmp/dual-or ]]; then
 curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/dual-or.txt' > /var/tmp/dual-or
 elif
-[[ $(find "/var/tmp/file2" -mmin +60 -print) ]]; then
+[[ $(find "/var/tmp/dual-or" -mmin +60 -print) ]]; then
 curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/dual-or.txt' > /var/tmp/dual-or
 fi
 cd /var/tmp
