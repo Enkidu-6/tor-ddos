c830a3897af2fd522c5add840199919a72d81028
Enkidu <101878139+Enkidu-6@users.noreply.github.com>
2022-11-08 20:40:24 -0500

    Update ip6tables.sh

--- ip6tables.sh
+++ ip6tables.sh
@@ -14,13 +14,10 @@ ipset add -exist allow-list6 2610:1c0:0:5::131
 ipset add -exist allow-list6 2620:13:4000:6000::1000:118
 ipset add -exist allow-list6 2a0c:dd40:1:b::42
 ipset create tor-ddos6 hash:ip family inet6 hashsize 4096 timeout 43200
-ipset create persec6 hash:ip family inet6 hashsize 4096 timeout 3600
 ip6tables -t mangle -I PREROUTING -p tcp -m set --match-set allow-list6 src -j ACCEPT
 ip6tables -t mangle -A PREROUTING -p tcp --syn --destination-port $ORPort -m hashlimit --hashlimit-name TOR6-$ORPort --hashlimit-mode srcip --hashlimit-srcmask 128 --hashlimit-above 1/minute --hashlimit-burst 5 --hashlimit-htable-expire 60000 -j DROP
 ip6tables -t mangle -A PREROUTING -p tcp --destination-port $ORPort -m recent --name tor-ddos6 --set
-ip6tables -t mangle -A PREROUTING -p tcp --syn --dport $ORPort -m conntrack --ctstate NEW -m hashlimit --hashlimit-name TOR6 --hashlimit-mode srcip --hashlimit-srcmask 128 --hashlimit-above 2/sec --hashlimit-burst 3 --hashlimit-htable-expire 3600 -j SET --add-set persec6 src
 ip6tables -t mangle -A PREROUTING -p tcp --destination-port $ORPort -m connlimit --connlimit-mask 128 --connlimit-above 4 -j SET --add-set tor-ddos6 src
 ip6tables -t mangle -A PREROUTING -p tcp --syn --destination-port $ORPort -m connlimit --connlimit-mask 128 --connlimit-above 4 -j DROP
 ip6tables -t mangle -A PREROUTING -p tcp -m set --match-set persec6 src -j DROP
-ip6tables -t mangle -A PREROUTING -p tcp -m set --match-set tor-ddos6 src -j DROP
 ip6tables -t mangle -A PREROUTING -p tcp --destination-port $ORPort -j ACCEPT
