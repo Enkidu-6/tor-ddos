abcf5eef80335fd09d8cd9998848741c5027733f
Enkidu <101878139+Enkidu-6@users.noreply.github.com>
2022-12-07 02:53:00 -0500

    Update refresh-authorities.sh

--- cron/refresh-authorities.sh
+++ cron/refresh-authorities.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # set -x
-ipset flush allow-list
-ipset flush allow-list6
 curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/authorities-v4.txt' | sed -e '1,3d' > /var/tmp/allow
 curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/snowflake.txt' | sed -e '1,3d' >> /var/tmp/allow
+curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/authorities-v6.txt' | sed -e '1,3d' > /var/tmp/allow6
+curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/snowflake-v6.txt' | sed -e '1,3d' >> /var/tmp/allow6
+ipset flush allow-list
+ipset flush allow-list6
 for i in `cat /var/tmp/allow` ;
 do
 /usr/sbin/ipset add -exist allow-list $i
 done;
-curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/authorities-v6.txt' | sed -e '1,3d' > /var/tmp/allow6
-curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/snowflake-v6.txt' | sed -e '1,3d' >> /var/tmp/allow6
 for i in `cat /var/tmp/allow6` ;
 do
 /usr/sbin/ipset add -exist allow-list6 $i
