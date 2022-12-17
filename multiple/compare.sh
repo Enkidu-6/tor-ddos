dd8646e04c948abb90cda0f4a7f8933fa7fbafa6
Enkidu <101878139+Enkidu-6@users.noreply.github.com>
2022-12-17 05:16:48 -0500

    Update compare.sh

--- multiple/compare.sh
+++ multiple/compare.sh
@@ -8,13 +8,13 @@ ipaddress2=10.1.1.3
 curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/relays-v4.txt' > /var/tmp/file2
 curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/dual-or.txt' > /var/tmp/dual-or
 cd /var/tmp
-echo -e "\033[0;32mAll relays in Block list $ipaddress1:\033[0m"
+echo -e "\033[1;32mAll relays in Block list $ipaddress1:\033[1;37m"
 perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  $ipaddress1 file2
-echo -e "\033[0;32mAll relays in Block list $ipaddress2:\033[0m"
+echo -e "\033[1;32mAll relays in Block list $ipaddress2:\033[1;37m"
 perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  $ipaddress2 file2
-echo -e "\033[0;32mRelays with two Tor instances in Block list $ipaddress1:\033[0m"
+echo -e "\033[1;32mRelays with two Tor instances in Block list $ipaddress1:\033[1;37m"
 perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  $ipaddress1 dual-or
-echo -e "\033[0;32mRelays with two Tor instances in Block list $ipaddress2:\033[0m"
+echo -e "\033[1;32mRelays with two Tor instances in Block list $ipaddress2:\033[1;37m"
 perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  $ipaddress2 dual-or
 
 read -p "Remove All 'a'. Only the ones with Dual ORPorts 'd'. Do Nothing 'n'  (a/d/n) " yn
@@ -46,9 +46,10 @@ case $yn in
               /usr/sbin/ipset del tor-$ipaddress2 $i
               done;;
             
-	n ) echo exiting...;
+	n ) echo -e "exiting.... \033[0m";
 	      exit;;
 
-	* ) echo invalid response;
+	* ) echo -e "invalid response \033[0m";
 	      exit 1;;
 esac
+echo -e "\033[0m"
