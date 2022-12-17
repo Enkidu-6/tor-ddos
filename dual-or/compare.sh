a3d46f12aca13db430af13ffe358f221fda59564
Enkidu <101878139+Enkidu-6@users.noreply.github.com>
2022-12-17 04:59:02 -0500

    Update compare.sh

--- dual-or/compare.sh
+++ dual-or/compare.sh
@@ -4,12 +4,12 @@ ipset -L tor-ddos | awk '{print $1}' > /var/tmp/file1
 curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/relays-v4.txt' > /var/tmp/file2
 curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/dual-or.txt' > /var/tmp/dual-or
 cd /var/tmp
-echo -e "\033[0;32mAll relays in Block list:\033[0m"
+echo -e "\033[1;32mAll relays in Block list:\033[1;37m"
 perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  file1 file2
-echo -e "\033[0;32mRelays with two Tor instances:\033[0m"
+echo -e "\033[1;32mRelays with two Tor instances:\033[1;37m"
 perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  file1 dual-or
 
-read -p "Remove All 'a'. Only the ones with Dual ORPorts 'd'. Do Nothing 'n'  (a/d/n) " yn
+read -p "Remove All 'a'. Only the ones with Dual OR ports 'd'. Do Nothing 'n'  (a/d/n) " yn
 
 case $yn in 
         a ) perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  file1 file2 > /var/tmp/remove ; 
@@ -26,9 +26,11 @@ case $yn in
               ipset del tor-ddos $i
               done;;
             
-	n ) echo exiting...;
+	n ) echo -e "exiting.... \033[0m";
 	      exit;;
 
-	* ) echo invalid response;
+	* ) echo -e "invalid response \033[0m";             
 	      exit 1;;
+
 esac
+echo -e "\033[0m"
