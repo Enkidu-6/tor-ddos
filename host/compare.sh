58789d0797916c0ec1963caf012d4131a418e545
Enkidu <101878139+Enkidu-6@users.noreply.github.com>
2022-12-19 21:44:59 -0500

    Update compare.sh

--- host/compare.sh
+++ host/compare.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 # set -x
 ipset -L tor-ddos | awk '{print $1}' > /var/tmp/file1
+if [[ ! -e /var/tmp/file2 ]]; then
 curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/relays-v4.txt' > /var/tmp/file2
+elif
+[[ $(find "/var/tmp/file2" -mmin +60 -print) ]]; then
+curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/relays-v4.txt' > /var/tmp/file2
+fi
+if [[ ! -e /var/tmp/dual-or ]]; then
 curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/dual-or.txt' > /var/tmp/dual-or
+elif
+[[ $(find "/var/tmp/dual-or" -mmin +60 -print) ]]; then
+curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/dual-or.txt' > /var/tmp/dual-or
+fi
 cd /var/tmp
 echo -e "\033[1;32mAll relays in Block list:\033[1;37m"
 perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  file1 file2
