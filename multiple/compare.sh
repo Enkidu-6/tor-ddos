14d4827b57a56dc537a970839706515c07ae1764
Enkidu <101878139+Enkidu-6@users.noreply.github.com>
2022-12-19 21:50:03 -0500

    Update compare.sh

--- multiple/compare.sh
+++ multiple/compare.sh
@@ -5,8 +5,18 @@ ipaddress1=10.1.1.2
 ipaddress2=10.1.1.3
 /usr/sbin/ipset -L tor-$ipaddress1 | awk '{print $1}' > /var/tmp/$ipaddress1
 /usr/sbin/ipset -L tor-$ipaddress2 | awk '{print $1}' > /var/tmp/$ipaddress2
+if [[ ! -e /var/tmp/file2 ]]; then
 curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/relays-v4.txt' > /var/tmp/file2
+elif
+[[ $(find "/var/tmp/file2" -mmin +60 -print) ]]; then
+curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/relays-v4.txt' > /var/tmp/file2
+fi
+if [[ ! -e /var/tmp/dual-or ]]; then
 curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/dual-or.txt' > /var/tmp/dual-or
+elif
+[[ $(find "/var/tmp/dual-or" -mmin +60 -print) ]]; then
+curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/dual-or.txt' > /var/tmp/dual-or
+fi
 cd /var/tmp
 echo -e "\033[1;32mAll relays in Block list $ipaddress1:\033[1;37m"
 perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  $ipaddress1 file2
