e8f16179824907cabbad30a4c7a0b7c414a2a183
Enkidu <101878139+Enkidu-6@users.noreply.github.com>
2022-12-19 21:52:52 -0500

    Update conntrack.sh

--- dual-or/conntrack.sh
+++ dual-or/conntrack.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 # set -x
+if [[ ! -e /var/tmp/file2 ]]; then
 curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/relays-v4.txt' > /var/tmp/file2
+elif
+[[ $(find "/var/tmp/file2" -mmin +60 -print) ]]; then
+curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/relays-v4.txt' > /var/tmp/file2
+fi
+if [[ ! -e /var/tmp/dual-or ]]; then
 curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/dual-or.txt' > /var/tmp/dual-or
+elif
+[[ $(find "/var/tmp/dual-or" -mmin +60 -print) ]]; then
+curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/dual-or.txt' > /var/tmp/dual-or
+fi
 echo -e "\033[1;32mConntrack count:\033[0m"
 echo -e "   \033[1;36m`cat /proc/sys/net/netfilter/nf_conntrack_count`\033[0m"
 echo -e "\033[1;32mIPs with more than 2 connections:\033[0m"
