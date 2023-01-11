d124461d716a0c481d936e900dd509475c4d54d8
Enkidu <101878139+Enkidu-6@users.noreply.github.com>
2023-01-10 23:18:45 -0500

    Update refresh-authorities.sh

--- refresh-authorities.sh
+++ refresh-authorities.sh
@@ -4,13 +4,26 @@ curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/authori
 curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/snowflake.txt' | sed -e '1,3d' >> /var/tmp/allow
 curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/authorities-v6.txt' | sed -e '1,3d' > /var/tmp/allow6
 curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/snowflake-v6.txt' | sed -e '1,3d' >> /var/tmp/allow6
-ipset flush allow-list
-ipset flush allow-list6
+curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/dual-or.txt' | sed -e '1,3d' > /var/tmp/multi
+curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/dual-or-v6.txt' | sed -e '1,3d' > /var/tmp/multi6
+/usr/sbin/ipset flush allow-list
 for i in `cat /var/tmp/allow` ;
 do
 /usr/sbin/ipset add -exist allow-list $i
 done;
+/usr/sbin/ipset flush allow-list6
 for i in `cat /var/tmp/allow6` ;
 do
 /usr/sbin/ipset add -exist allow-list6 $i
 done;
+/usr/sbin/ipset flush dual-or
+for i in `cat /var/tmp/multi` ;
+do
+/usr/sbin/ipset add -exist dual-or $i
+done;
+/usr/sbin/ipset flush dual-or6
+for i in `cat /var/tmp/multi6` ;
+do
+/usr/sbin/ipset add -exist dual-or6 $i
+done;
+/bin/rm -r /var/tmp/allow /var/tmp/allow6 /var/tmp/multi /var/tmp/multi6
