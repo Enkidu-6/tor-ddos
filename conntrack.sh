f143619a0957ab61f251679914ff2e2c9bd1bd1c
Enkidu <101878139+Enkidu-6@users.noreply.github.com>
2023-02-15 20:55:27 -0500

    Update conntrack.sh

--- conntrack.sh
+++ conntrack.sh
@@ -4,8 +4,8 @@ if [[ ! -e /proc/net/nf_conntrack ]]; then
 clear
 echo -e "\033[1;32mYour system does not come with nf_conntrack. Please install conntrack utilities\nif you don't already have it. 'apt install conntrack' and use conntrack-2.sh\033[0m"
 exit 1
-
 fi
+
 if [[ ! -e /var/tmp/file2 ]]; then
 curl -s 'https://raw.githubusercontent.com/Enkidu-6/tor-relay-lists/main/relays-v4.txt' > /var/tmp/file2
 elif
@@ -45,22 +45,22 @@ echo -e "\033[1;37m`cat 5 | grep -v ' 1 ' | grep -v ' 2 '`\033[0m"
 echo -e "\033[1;32mIPs with More than Two connections:\033[0m"
 cat 5 | grep -v ' 1 ' | grep -v ' 2 ' | awk '{ print $2 }' > 8
 
-echo -e "\033[1;37m There are \033[1;36m`cat 8 | wc -l`\033[1;37m IPs" 
-echo -e "\033[1;36m `perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  8 file2 | wc -l` \033[1;37mRelays" 
-echo -e "\033[1;36m `perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  8 multi-or | wc -l` \033[1;37mmulti-OR"
-echo -e "\033[1;36m `perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  8 snow | wc -l` \033[1;37msnowflake\033[0m" 
+echo -e "\033[1;37m There are \033[1;36m`cat 8 | wc -l`\033[1;37m IPs With More than Two connections" 
+echo -e "\033[1;36m           `perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  8 file2 | wc -l` \033[1;37mRelays" 
+echo -e "\033[1;36m           `perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  8 multi-or | wc -l` \033[1;37mmulti-OR"
+echo -e "\033[1;36m           `perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  8 snow | wc -l` \033[1;37msnowflake\033[0m" 
 
 echo -e "\033[1;32mIPs with Two connections:\033[0m"
 cat 5 | grep ' 2 ' | awk '{ print $2 }' > 6 
 
-echo -e "\033[1;37m There are \033[1;36m`cat 6 | wc -l`\033[1;37m IPs"
-echo -e "\033[1;36m `perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  6 file2 | wc -l` \033[1;37mRelays\033[0m"
-echo -e "\033[1;36m `perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  6 dual-or | wc -l` \033[1;37mDual-OR\033[0m"
+echo -e "\033[1;37m There are \033[1;36m`cat 6 | wc -l`\033[1;37m IPs With Two connections"
+echo -e "\033[1;36m           `perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  6 file2 | wc -l` \033[1;37mRelays\033[0m"
+echo -e "\033[1;36m           `perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  6 dual-or | wc -l` \033[1;37mDual-OR\033[0m"
 
 echo -e "\033[1;32mIPs with One connection:\033[0m"
 cat 5 | grep ' 1 ' | awk '{ print $2 }' > 7
 
-echo -e "\033[1;37m There are \033[1;36m`cat 7 | wc -l`\033[1;37m IPs"
-echo -e "\033[1;36m `perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  7 file2 | wc -l` \033[1;37mRelays"
-echo -e "\033[1;36m `perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  7 dual-or | wc -l` \033[1;37mDual-OR\033[0m"
+echo -e "\033[1;37m There are \033[1;36m`cat 7 | wc -l`\033[1;37m IPs With One connection"
+echo -e "\033[1;36m           `perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  7 file2 | wc -l` \033[1;37mRelays"
+echo -e "\033[1;36m           `perl -ne 'print if ($seen{$_} .= @ARGV) =~ /10$/'  7 dual-or | wc -l` \033[1;37mDual-OR\033[0m"
 /bin/rm -r 5 6 7 8 snow
