eddcce5058485379f9395ebe2377f1dcaf5ebabc
Enkidu <101878139+Enkidu-6@users.noreply.github.com>
2024-03-10 17:22:04 +0000

    v7.0.2

--- download.sh
+++ download.sh
@@ -2,6 +2,7 @@
 # set -x
 red='\033[0;31m'
 green='\033[1;32m'
+white='\033[1;37m'
 plain='\033[0m'
 wget -O tor.tar.gz https://github.com/Enkidu-6/tor-ddos/archive/refs/tags/v7.0.2.tar.gz
 mkdir tor
@@ -10,51 +11,51 @@ sleep 1
 cd tor
 chmod 0700 *.sh
 check() {
-FILE=$(find / -name ipv4.txt)
-FILE2=$(find / -name ipv6.txt)
-if [ -f "$FILE" ]; then
-    echo -e ${green}"Looks like you may have used these scripts before."
-    echo -e "You can upgrade to the new version and keep your block lists intact"
-    read -p "Would you like to update your existing rules? <y/n> " prompt
-    echo -e ${plain}
-    case $prompt in
-    [yY]*)
-        echo -e ${green}"Great! This is what I found in your current IP files"${plain}
-        echo -e "${white}$(cat $FILE) ${plain}\n"
-        echo -e "${white}$(cat $FILE2) ${plain}\n"
-        echo -e ${green}
-        read -p "Are these values correct? <y/n> " response
+    FILE=$(find / -name ipv4.txt)
+    FILE2=$(find / -name ipv6.txt)
+    if [ -f "$FILE" ]; then
+        echo -e ${green}"Looks like you may have used these scripts before."
+        echo -e "You can upgrade to the new version and keep your block lists intact"
+        read -p "Would you like to update your existing rules? <y/n> " prompt
         echo -e ${plain}
-        case $response in
+        case $prompt in
         [yY]*)
-            ./update.sh
+            echo -e "\n${green}Great! This is what I found in your current IP files: ${plain}\n\n"
+            echo -e "${white}$(cat $FILE) ${plain}\n"
+            echo -e "${white}$(cat $FILE2) ${plain}\n"
+            echo -e ${green}
+            read -p "Are these values correct? <y/n> " response
+            echo -e ${plain}
+            case $response in
+            [yY]*)
+                ./update.sh
+                ;;
+
+            [nN]*)
+                echo -e ${green} "Okay then, we start fresh" ${plain}
+                ./start.sh
+                ;;
+
+            *)
+                echo -e ${red}"Invalid Response"${plain}
+                check
+                ;;
+
+            esac
             ;;
 
         [nN]*)
-            echo -e ${green} "Okay then, we start fresh" ${plain}
+            echo -e ${green}"Okay. We'll start fresh."${plain}
             ./start.sh
             ;;
 
         *)
-            echo -e ${red}"Invalid Response"${plain}
+            echo -e ${red}"Invalid response"${plain}
             check
             ;;
-
         esac
-        ;;
-
-    [nN]*)
-        echo -e ${green}"Okay. We'll start fresh."${plain}
+    else
         ./start.sh
-        ;;
-
-    *)
-        echo -e ${red}"Invalid response"${plain}
-        check
-        ;;
-    esac
-else
-    ./start.sh
-fi
+    fi
 }
 check
